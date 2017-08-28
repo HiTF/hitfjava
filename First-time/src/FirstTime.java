3ef6aebe20589b29fb6ae63a1b81f29caf09ce7f
Ilya Ryakin <ris2012.r@yandex.ru>
2017-08-29 02:09:17 +0300

    lol

--- First-time/src/FirstTime.java
+++ First-time/src/FirstTime.java
@@ -1,11 +1,9 @@
 
-class HelloWorld {
+class FirstTime {
 
 	public static void main(String[] args) {
-		System.out.println("I love Java");
-		
 		// TODO Автоматически созданная заглушка метода
 
 	}
 
-}ertertertertert
+}
