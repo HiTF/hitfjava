b4529a5bac4be43cc39ca028272f512395d5a6fd
Ilya Ryakin <ris2012.r@yandex.ru>
2017-08-29 14:20:25 +0300

    1.1

--- First-time/src/FirstTime.java
+++ First-time/src/FirstTime.java
@@ -1,9 +1,14 @@
+import javax.swing.JFrame;
 
 class FirstTime {
 
 	public static void main(String[] args) {
-		// TODO Автоматически созданная заглушка метода
+		JFrame myFrame = new JFrame();
+		String myTitle = "Upyachka";
+		myFrame.setTitle(myTitle);
+		myFrame.setSize(300, 200);
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		System.out.println("Hello World!");
 	}
-
 }
+
